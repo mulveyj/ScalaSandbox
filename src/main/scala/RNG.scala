8e13685ca77afa107f031e9fe2bd982a3147a77e
Joe Mulvey <joe.mulvey@autotrader.co.uk>
2018-11-01 19:59:21 +0000

    Random Number Generator and flatMap

--- src/main/scala/RNG.scala
+++ src/main/scala/RNG.scala
@@ -26,15 +26,7 @@ object RNG {
 
   def double(rng: RNG): (Double, RNG) = {
     val (int1, rng1) = RNG.nonnegativeInt(rng)
-    def limitedDouble(value: Double, place: Int): Double = {
-      value match {
-        case k if k == Int.MaxValue => 0.99999999999999
-        case k if k >= 1 => limitedDouble(k / 10, place + 1)
-        case k if k < 1 => roundDecimal(k, place + 1)
-      }
-    }
-
-    (limitedDouble(int1.toDouble, 0), rng1)
+    (int1.toDouble / (Int.MaxValue.toDouble + 1), rng1)
   }
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = {
@@ -43,7 +35,7 @@ object RNG {
     ((n1, n2), rng2)
   }
 
-  def ints(count: Integer)(rng: RNG): (List[Int], RNG) = {
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
     def getNext(rng1: RNG, n:Int, l: List[Int]): (List[Int], RNG) = {
       if (n < count) {
         val (num, r) = rng1.nextInt
@@ -57,6 +49,61 @@ object RNG {
     getNext(rng, 0, List())
   }
 
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def double2(rng: RNG): Rand[Double] = {
+    map(nonnegativeInt)(num => num / (Int.MaxValue.toDouble + 1))
+  }
+
+  def map2[A, B, C](rng1: Rand[A], rng2: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rng3) = rng1(rng)
+      val (b, rng4) = rng2(rng3)
+      (f(a, b), rng4)
+    }
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
+
+  def sequence[A](rs: List[Rand[A]]): Rand[List[A]] = {
+    rs.foldRight(unit(List.empty[A]))((elem, acc) => map2(elem, acc)((p1: A, p2: List[A]) => List(p1) ++ p2))
+  }
+
+  def ints2(count: Int)(rng: RNG): Rand[List[Int]] = {
+    val listOfRNGs = (for (i <- 0 to count) yield (r: RNG) => r.nextInt).toList
+
+    sequence(listOfRNGs)
+  }
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    (r0: RNG) => {
+      val (a, r) = f(r0)
+      g(a)(r)
+    }
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    def retryMod(i: Int) = {
+      val mod = i % n
+      if (i + n - 1 - mod >= 0)
+        RNG.unit(mod)
+      else RNG.nonNegativeLessThan(n)
+    }
+    flatMap(nonnegativeInt)(retryMod)
+  }
+
+  def map3[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+    flatMap(s)(a => unit(f(a)))
+  }
 
 //  helper
   def roundDecimal(number: Double, places: Int): Double = {
--- src/test/scala/RNGTest.scala
+++ src/test/scala/RNGTest.scala
@@ -1,5 +1,6 @@
+import RNG.Rand
 import org.scalatest.FunSuite
-import scala.math._
+
 
 class RNGTest extends FunSuite {
   test("Simple nextInt") {
@@ -25,8 +26,8 @@ class RNGTest extends FunSuite {
     val rng0 = RNG.Simple(seed)
     val (n1, rng1) = RNG.double(rng0)
     val (n2, rng2) = RNG.double(rng1)
-    assert(n1 == 0.16159453)
-    assert(n2 == 0.1281479697)
+    assert(n1 == 0.007524831686168909)
+    assert(n2 == 0.5967354853637516)
   }
 
   test("intDouble") {
@@ -34,7 +35,7 @@ class RNGTest extends FunSuite {
     val rng0 = RNG.Simple(seed)
     val (intDbl, r) = RNG.intDouble(rng0)
     assert(intDbl._1 == 16159453)
-    assert(intDbl._2 == 0.1281479697)
+    assert(intDbl._2 == 0.5967354853637516)
   }
 
   test("rand List") {
@@ -44,4 +45,54 @@ class RNGTest extends FunSuite {
     val expected = List(1538995, -322738770, 1561020537, -119123850, 358254227)
     assert(rl == expected)
   }
+
+//  test("double2") {
+//    val seed = 42
+//    val rng0 = RNG.Simple(seed)
+//    val (n1, rng1) = RNG.double2(rng0)
+//    assert(n1 == 0.007524831686168909)
+//    assert(n2 == 0.5967354853637516)
+//  }
+
+  test("ints2") {
+    val seed = 4
+    val rng0 = RNG.Simple(seed)
+    val (n1, r2) = rng0.nextInt
+    val rl: Rand[List[Int]] = RNG.ints2(5)(rng0)
+    val expected = rl(r2)._1
+    assert(expected.isInstanceOf[List[Int]])
+    assert(expected == List(-322738770, 1561020537, -119123850, 358254227, 784223886, -158151461))
+  }
+
+  test("flatMap") {
+    val seed = 4
+    val rng0 = RNG.Simple(seed)
+    val f = (r: RNG) => r.nextInt
+    val g = (i: Int) => {(r: RNG) => (i.toDouble / 10, r)}
+    val fm = RNG.flatMap(f)(g)
+    val d = fm(rng0)._1
+    val expected = 153899.5
+    assert(d == expected)
+  }
+
+  test("nonNegativeLessThan") {
+    val seed = 4
+    val rng0 = RNG.Simple(seed)
+    val maxval = 9
+    val fm = RNG.nonNegativeLessThan(maxval)
+    val d = fm(rng0)._1
+    assert(d < maxval)
+  }
+
+  test("map in terms of flatMap") {
+    val seed = 4
+    val rng0 = RNG.Simple(seed)
+    val f = (i: Int) => i.toDouble / 10
+    val s = (r: RNG) => r.nextInt
+    val m = RNG.map(s)(f)
+    val expected = 153899.5
+    assert(m(rng0)._1 == expected)
+  }
+
+
 }
